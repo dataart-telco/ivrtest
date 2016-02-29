7bc197de054fcc8de65ea2e02fb5a2e0c139ba8f
Gennadiy Dubina <gdubina@dataart.com>
2016-02-29 22:25:15 +0200

    improve ab

--- run_ab.sh
+++ run_ab.sh
@@ -2,4 +2,8 @@
 
 echo -n "Digits=7" > /tmp/req_gather.data
 
-ab -n 100000 -c 1000 -p /tmp/req_gather.data -T 'application/x-www-form-urlencoded' 127.0.0.1:7090/gather 
+curl 127.0.0.1:7090/start
+ab -n 100000 -c 500 127.0.0.1:7090/incoming &>/dev/null &
+ab -n 100000 -c 500 -p /tmp/req_gather.data -T 'application/x-www-form-urlencoded' 127.0.0.1:7090/gather
+
+curl 127.0.0.1:7090
